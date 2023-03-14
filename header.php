67f7909912db5382304404c28f19f5975535bb24
cuelly3 <63018899+cuelly3@users.noreply.github.com>
2023-03-14 23:43:21 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,10 +4,6 @@ $username = "root";
 $password = "admin";
 $dbname = "highsocials";
 
-/*$servername = "188.241.222.25";
-$username = "highsoci";
-$password = "2Mfh8s9[Y!XU5q";
-$dbname = "highsoci_db";*/
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
